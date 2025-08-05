8fada91bb24db45c5f113a51578f9ef2c45ac734
Le0-dot <lkolyadich@gmail.com>
2025-08-05 16:07:22 +0200

    Move files around

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
-	},
-	build = ':TSUpdate',
-	opts = {
-		ensure_installed = { "cpp", "python", "robot", "haskell", },
-		auto_install = true,
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+    opts = {
+        ensure_installed = { "lua", "python", "robot" },
+        auto_install = true,
 
-		highlight = { enable = true },
-		indent = { enable = true },
-	}
+        highlight = { enable = true },
+        indent = { enable = true },
+    }
 }
