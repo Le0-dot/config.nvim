e941d4d77c7478acf9f4c59ee2c7586136e592f2
Le0-dot <lkolyadich@gmail.com>
2025-08-06 12:48:19 +0200

    Remove unused plugin

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -4,10 +4,6 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make'
-        },
         'nvim-telescope/telescope-ui-select.nvim',
     },
     event = 'VeryLazy',
@@ -47,7 +43,6 @@ return {
     },
     config = function()
         require('telescope').setup()
-        require('telescope').load_extension('fzf')
         require('telescope').load_extension('ui-select')
     end
 }
