5f119e41456e9a9f79b71ccc024355aad38983ff
Le0-dot <lkolyadich@gmail.com>
2025-08-06 11:26:05 +0200

    Change status line format

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,7 +1,17 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = {
-		'kyazdani42/nvim-web-devicons',
-	},
-	opts = {}
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+        'kyazdani42/nvim-web-devicons',
+    },
+    opts = {
+        options = { section_separators = '', component_separators = '│' },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff' },
+            lualine_c = { { 'filename', path = 1 }, },
+            lualine_x = { 'filetype' },
+            lualine_y = { 'lsp_status' },
+            lualine_z = { 'searchcount', 'location' }
+        },
+    }
 }
