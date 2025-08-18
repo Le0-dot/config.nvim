64e913eaaf478a72a315211fe0e26f8f20cd7af6
Le0-dot <lkolyadich@gmail.com>
2025-08-18 17:24:21 +0200

    Check if lsp supports formatting before enabling autoformatting

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -94,15 +94,18 @@ vim.lsp.enable(
 )
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function()
+    callback = function(args)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
 
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-        })
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client:supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
+        end
     end
 })
 
