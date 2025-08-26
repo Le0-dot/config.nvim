4530cce7362373bf500e743cd9be0df81f43127e
Le0-dot <lkolyadich@gmail.com>
2025-08-26 16:04:36 +0200

    Make DEBUG 'mode' to show in lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,15 +3,35 @@ return {
     dependencies = {
         'kyazdani42/nvim-web-devicons',
     },
-    opts = {
-        options = { section_separators = '', component_separators = '│' },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff' },
-            lualine_c = { { 'filename', path = 1 }, },
-            lualine_x = { 'filetype' },
-            lualine_y = { 'diagnostics', 'lsp_status' },
-            lualine_z = { 'searchcount', 'location' }
-        },
-    }
+    config = function()
+        local dmode_enabled = false
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "DebugModeChanged",
+            callback = function(args)
+                dmode_enabled = args.data.enabled
+            end
+        })
+        require('lualine').setup({
+            options = { section_separators = '', component_separators = '│' },
+            sections = {
+                lualine_a = {
+                    {
+                        'mode',
+                        cond = function() return not dmode_enabled end,
+                    },
+                    {
+                        '"DEBUG"',
+                        cond = function()
+                            return dmode_enabled
+                        end
+                    }
+                },
+                lualine_b = { 'branch', 'diff' },
+                lualine_c = { { 'filename', path = 1 }, },
+                lualine_x = { 'filetype' },
+                lualine_y = { 'diagnostics', 'lsp_status' },
+                lualine_z = { 'searchcount', 'location' }
+            },
+        })
+    end
 }
