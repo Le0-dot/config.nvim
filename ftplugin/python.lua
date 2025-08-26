ac9a9ce629595231d8bea5b649b998698a3ad2a2
Le0-dot <lkolyadich@gmail.com>
2025-08-26 16:05:44 +0200

    Add autoimport of debugpy in python file when entering debug mode

---
+++ ftplugin/python.lua
@@ -0,0 +1,22 @@
+local function buf_start_toggle(lines, present)
+    local buf = 0
+    local len = #lines
+
+    local actual = vim.api.nvim_buf_get_lines(buf, 0, len, false)
+    local equals = vim.deep_equal(lines, actual)
+
+    if not equals and present then
+        vim.api.nvim_buf_set_lines(buf, 0, 0, false, lines)
+    elseif equals and not present then
+        vim.api.nvim_buf_set_lines(buf, 0, len, false, {})
+    end
+end
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "DebugModeChanged",
+    callback = function(args)
+        local lines = { '#fmt: off', 'import debugpy', 'debugpy.listen(5678)', 'debugpy.wait_for_client()', '#fmt: on' }
+        buf_start_toggle(lines, args.data.enabled)
+        vim.cmd([[ :write ]])
+    end
+})
