b179ed08ac4298d6a05e5c115a3d041c9e0f8c7b
Le0-dot <lkolyadich@gmail.com>
2025-11-17 17:03:29 +0100

    configure oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,11 +3,17 @@ return {
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     lazy = false,
     keys = {
-        { "n", "<leader>ed", function() vim.cmd.edit("%:h") end, desc = "[E]dit [d]irectory" },
+        { "<leader>ed", function() vim.cmd.edit("%:h") end, desc = "[E]dit [d]irectory" },
     },
-    ---@module 'oil'
-    ---@type oil.SetupOpts
     opts = {
         default_file_explorer = true,
+        buf_options = {
+            buflisted = false,
+            bufhidden = "hide",
+        },
+        delete_to_trash = true,
+        view_options = {
+            show_hidden = true,
+        },
     },
 }
