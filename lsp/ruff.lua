e81d417f68ec8ab4440d6e5cbce18928d3658199
Le0-dot <lkolyadich@gmail.com>
2026-01-14 10:26:53 +0100

    make ruff prefer .git repos over python project

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,6 +1,6 @@
 return {
     cmd = { 'ruff', 'server' },
     filetypes = { 'python' },
-    root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+    root_markers = { '.git', 'pyproject.toml', 'ruff.toml', '.ruff.toml' },
     settings = {},
 }
