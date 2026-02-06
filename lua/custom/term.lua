4212650d8699954cc3b1ab63ca35be9fea71f425
Le0-dot <lkolyadich@gmail.com>
2026-02-06 17:43:36 +0100

    fix annoying tab behaviour

--- lua/custom/term.lua
+++ lua/custom/term.lua
@@ -6,13 +6,12 @@ vim.keymap.set(
 vim.keymap.set(
     { 'n', 'i', 't' }, '<M-g>', function()
         vim.cmd([[$tab term lazygit]])
-        local buf = vim.api.nvim_get_current_buf()
-        vim.api.nvim_create_autocmd('BufLeave',
-            {
-                buffer = buf,
-                callback = function(args) vim.api.nvim_buf_delete(args.buf, { force = true }) end,
-            }
-        )
+
+        -- Try closing when entering another tab
+        vim.api.nvim_create_autocmd('TabEnter', {
+            callback = function() pcall(vim.cmd.tabclose, '#') end,
+            once = true,
+        })
 
         -- Works for some reason
         vim.o.cmdheight = 0
@@ -46,11 +45,27 @@ vim.api.nvim_create_autocmd('TermOpen', {
     end
 })
 
--- Enter TERMINAL mode on entering term window
-vim.api.nvim_create_autocmd('TermOpen', { command = 'startinsert' })
-vim.api.nvim_create_autocmd('BufEnter', {
+-- Remove statusline in terminal windows
+vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
     pattern = 'term://*',
-    command = 'startinsert',
+    callback = function() vim.opt.laststatus = 0 end
+})
+
+-- Restore statusline on leaving terminal windows
+vim.api.nvim_create_autocmd('BufLeave', {
+    pattern = 'term://*',
+    callback = function() vim.opt.laststatus = 2 end
+})
+
+-- Enter TERMINAL mode on entering term window
+vim.api.nvim_create_autocmd('TabLeave', {
+    callback = function()
+        vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter', 'SafeState' }, {
+            pattern = 'term://*',
+            command = 'startinsert',
+            once = true,
+        })
+    end
 })
 
 -- Skip the exit code
@@ -58,3 +73,140 @@ vim.api.nvim_create_autocmd('TermClose', {
     pattern = 'term://*',
     callback = function(_) vim.api.nvim_input('<CR>') end
 })
+
+
+
+
+
+
+-- Events = {
+--     'BufAdd',
+--     'BufDelete',
+--     'BufEnter',
+--     'BufFilePost',
+--     'BufFilePre',
+--     'BufHidden',
+--     'BufLeave',
+--     'BufModifiedSet',
+--     'BufNew',
+--     'BufNewFile',
+--     'BufRead',
+--     'BufReadPost',
+--     'BufReadCmd',
+--     'BufReadPre',
+--     'BufUnload',
+--     'BufWinEnter',
+--     'BufWinLeave',
+--     'BufWipeout',
+--     'BufWrite',
+--     'BufWritePre',
+--     'BufWriteCmd',
+--     'BufWritePost',
+--     'ChanInfo',
+--     'ChanOpen',
+--     'CmdUndefined',
+--     'CmdlineChanged',
+--     'CmdlineEnter',
+--     'CmdlineLeave',
+--     'CmdwinEnter',
+--     'CmdwinLeave',
+--     'ColorScheme',
+--     'ColorSchemePre',
+--     'CompleteChanged',
+--     'CompleteDonePre',
+--     'CompleteDone',
+--     'CursorHold',
+--     'CursorHoldI',
+--     'CursorMoved',
+--     'CursorMovedC',
+--     'CursorMovedI',
+--     'DiffUpdated',
+--     'DirChanged',
+--     'DirChangedPre',
+--     'ExitPre',
+--     'FileAppendCmd',
+--     'FileAppendPost',
+--     'FileAppendPre',
+--     'FileChangedRO',
+--     'FileChangedShell',
+--     'FileChangedShellPost',
+--     'FileReadCmd',
+--     'FileReadPost',
+--     'FileReadPre',
+--     'FileType',
+--     'FileWriteCmd',
+--     'FileWritePost',
+--     'FileWritePre',
+--     'FilterReadPost',
+--     'FilterReadPre',
+--     'FilterWritePost',
+--     'FilterWritePre',
+--     'FocusGained',
+--     'FocusLost',
+--     'FuncUndefined',
+--     'UIEnter',
+--     'UILeave',
+--     'InsertChange',
+--     'InsertCharPre',
+--     'InsertEnter',
+--     'InsertLeavePre',
+--     'InsertLeave',
+--     'MenuPopup',
+--     'ModeChanged',
+--     'OptionSet',
+--     'QuickFixCmdPre',
+--     'QuickFixCmdPost',
+--     'QuitPre',
+--     'RemoteReply',
+--     'SearchWrapped',
+--     'RecordingEnter',
+--     'RecordingLeave',
+--     'SafeState',
+--     'SessionLoadPost',
+--     'SessionWritePost',
+--     'ShellCmdPost',
+--     'Signal',
+--     'ShellFilterPost',
+--     'SourcePre',
+--     'SourcePost',
+--     'SourceCmd',
+--     'SpellFileMissing',
+--     'StdinReadPost',
+--     'StdinReadPre',
+--     'SwapExists',
+--     'Syntax',
+--     'TabEnter',
+--     'TabLeave',
+--     'TabNew',
+--     'TabNewEntered',
+--     'TabClosed',
+--     'TermOpen',
+--     'TermEnter',
+--     'TermLeave',
+--     'TermClose',
+--     'TermRequest',
+--     'TermResponse',
+--     'TextChanged',
+--     'TextChangedI',
+--     'TextChangedP',
+--     'TextChangedT',
+--     'TextYankPost',
+--     'User',
+--     'VimEnter',
+--     'VimLeavePre',
+--     'VimResume',
+--     'VimSuspend',
+--     'WinClosed',
+--     'WinEnter',
+--     'WinLeave',
+--     'WinNew',
+--     'WinScrolled',
+--     'WinResized',
+-- }
+--
+--
+-- vim.api.nvim_create_autocmd(Events, {
+--     callback = function(args)
+--         print(args.buf .. ' ' .. args.event .. ' ' .. args.file .. '\n')
+--     end
+-- })
