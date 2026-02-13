c49520f509f94b05b81337fe1fc0211c6e3a66bb
Le0-dot <lkolyadich@gmail.com>
2026-02-13 14:04:43 +0100

    simplify session management

--- lua/custom/session.lua
+++ lua/custom/session.lua
@@ -1,14 +1,18 @@
 vim.api.nvim_create_user_command("WithSession", function(data)
-    local path = #data.fargs > 0 and data.fargs[1] or vim.fn.getcwd()
-    local file = #data.fargs > 1 and data.fargs[2] or "Session.vim"
-
-    vim.cmd.cd(path)
-
-    if vim.fn.filereadable(file) == 0 then
-        vim.cmd.mksession(file)
+    if vim.fn.filereadable(data.args) == 0 then
+        vim.cmd.cd(vim.fs.dirname(data.args))
+        vim.cmd.mksession(data.args)
     end
 
-    vim.cmd.source(file)
+    vim.cmd.source(data.args)
+end, { desc = "Load or create session in specified path", nargs = 1 })
 
-    vim.api.nvim_create_autocmd("VimLeavePre", { command = "mksession! " .. file })
-end, { desc = "Open neovim with session in specified directory", nargs = "*" })
+vim.api.nvim_create_autocmd("SessionLoadPost", {
+    desc = "Schedule session write on exit",
+    callback = function()
+        vim.api.nvim_create_autocmd("VimLeavePre", {
+            desc = 'Write session on exit',
+            command = 'mksession! ' .. vim.v.this_session,
+        })
+    end,
+})
