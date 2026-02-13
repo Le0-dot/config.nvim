b53078b0b3d8bfe478866db72d99247dd45ea6ec
Le0-dot <lkolyadich@gmail.com>
2026-02-13 16:47:35 +0100

    make inner instance take the same tabpage

--- lua/custom/instance.lua
+++ lua/custom/instance.lua
@@ -1,22 +1,29 @@
 function HostEdit(sock_path, flags, files)
-    local initial_tabpage = vim.api.nvim_get_current_tabpage()
+    local buffer = vim.api.nvim_get_current_buf()
+    vim.bo[buffer].bufhidden = 'hide'
 
-    vim.cmd.tabedit(files[1])
-    vim.iter(files)
-        :skip(1)
-        :each(
-            vim.list_contains(flags, '-d') and vim.cmd.diffsplit or vim.cmd.vsplit
-        )
+    local edit = files[1] and vim.cmd.edit or vim.cmd.enew
+    local split = vim.list_contains(flags, '-d') and vim.cmd.diffsplit or vim.cmd.vsplit
 
-    local instance_position = vim.api.nvim_tabpage_get_number(0)
+    edit(files[1])
+    vim.iter(files):skip(1):each(split)
+
+    vim.iter(vim.api.nvim_tabpage_list_wins(0))
+        :map(vim.api.nvim_win_get_buf)
+        :each(function(buf) vim.bo[buf].bufhidden = 'wipe' end)
+
+    vim.o.eventignore = 'TabLeave'
 
     vim.api.nvim_create_autocmd("TabClosed", {
-        pattern = tostring(instance_position),
+        pattern = tostring(vim.api.nvim_tabpage_get_number(0)),
         once = true,
         callback = function()
-            if vim.api.nvim_tabpage_is_valid(initial_tabpage) then
-                vim.api.nvim_set_current_tabpage(initial_tabpage)
-            end
+            vim.cmd.tabnew()
+            vim.cmd.buffer(buffer)
+            vim.cmd.startinsert()
+
+            vim.bo[buffer].bufhidden = ''
+            vim.o.eventignore = ''
 
             local sock = vim.fn.sockconnect("pipe", sock_path, { rpc = true })
             vim.fn.rpcnotify(sock, "nvim_command", "quit!")
@@ -31,7 +38,9 @@ if not sock_path then
 end
 
 local files = vim.fn.argv()
-local flags = vim.iter(vim.v.argv):filter(function(arg) return vim.startswith(arg, '-') end):totable()
+local flags = vim.iter(vim.v.argv)
+    :filter(function(arg) return vim.startswith(arg, '-') end)
+    :totable()
 
 local rpc_sock = vim.fn.serverlist()[1] or vim.fn.serverstart()
 
