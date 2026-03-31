39b811062a4ebe45ee24f00d23655a1857619404
Le0-dot <lkolyadich@gmail.com>
2026-03-31 16:57:11 +0200

    fix gyf and gyd for oil buffers

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -15,5 +15,28 @@ return {
         view_options = {
             show_hidden = true,
         },
+        keymaps = {
+            ["gyf"] = {
+                mode = 'n',
+                callback = function()
+                    local cwd = vim.fn.getcwd()
+                    local dir = require("oil").get_current_dir(0)
+                    local file = require("oil").get_cursor_entry().name
+                    local relative = vim.fs.relpath(cwd, vim.fs.joinpath(dir, file))
+                    vim.fn.setreg("", relative)
+                    vim.fn.setreg("+", relative)
+                end
+            },
+            ["gyd"] = {
+                mode = 'n',
+                callback = function()
+                    local cwd = vim.fn.getcwd()
+                    local dir = require("oil").get_current_dir(0)
+                    local relative = vim.fs.relpath(cwd, dir)
+                    vim.fn.setreg("", relative)
+                    vim.fn.setreg("+", relative)
+                end
+            },
+        },
     },
 }
