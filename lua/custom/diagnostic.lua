b1530a8a9baf0dfc1d49ce7963fb1f227607bb0a
Le0-dot <lkolyadich@gmail.com>
2026-04-02 16:16:59 +0200

    remove popup with diagnostic

--- lua/custom/diagnostic.lua
+++ lua/custom/diagnostic.lua
@@ -1,7 +1,7 @@
 -- vim.diagnostic.config({ virtual_lines = { current_line = true } })
 vim.diagnostic.config({ float = { scope = 'cursor' } })
 
-vim.api.nvim_create_autocmd('CursorHold', {
-    desc = 'Show diagnostic window for current line',
-    callback = function() vim.diagnostic.open_float() end,
-})
+-- vim.api.nvim_create_autocmd('CursorHold', {
+--     desc = 'Show diagnostic window for current line',
+--     callback = function() vim.diagnostic.open_float() end,
+-- })
