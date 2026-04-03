37822c935f17530db4784149c617cbc2587e0a37
Le0-dot <lkolyadich@gmail.com>
2026-04-03 12:46:03 +0200

    implement auto_install option from nvim-treesitter master

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -5,8 +5,16 @@ vim.pack.add({
 
 vim.cmd.TSUpdate()
 
-    -- opts = {
-    --     auto_install = true,
-    --     highlight = { enable = true },
-    --     indent = { enable = true },
-    -- }
+vim.api.nvim_create_autocmd('FileType', {
+    desc = 'Install treesitter parsers when opening new filetypes',
+    callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        local treesitter = require('nvim-treesitter')
+        local installed = vim.list_contains(treesitter.get_installed(), lang)
+        local available = vim.list_contains(treesitter.get_available(), lang)
+
+        if not installed and available then
+            treesitter.install({ lang }, { summary = true })
+        end
+    end
+})
